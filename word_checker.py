a5d43930527f180ae44555cd01a117ecbb3ffa0f
tendaitt <970497+tendaitt@users.noreply.github.com>
2020-04-17 12:10:41 +0200

    Add get_common_words(), get_test_words() functions

--- word_checker.py
+++ word_checker.py
@@ -1,13 +1,42 @@
 import csv
 
-common_words = set()
+from docx import Document
+from string import punctuation
 
-with open('common_words.csv', 'r') as common_words_file:
 
-    reader = csv.reader(common_words_file)
+def get_common_words():
 
-    for row in reader:
-        common_words.update(row)
+    common_words = set()
 
-for word in common_words:
-    print(word)
+    with open('common_words.csv', 'r') as common_words_file:
+
+        reader = csv.reader(common_words_file)
+
+        for row in reader:
+            common_words.update(row)
+
+        return common_words
+
+
+def get_test_words():
+
+    test_words = []
+    word_doc = Document("test.docx")
+
+    num_paragraphs = len(word_doc.paragraphs)
+
+    for paragraph in range(0, num_paragraphs):
+        text = word_doc.paragraphs[paragraph].text
+
+        words = text.translate(str.maketrans('', '', punctuation)).split()
+        test_words = test_words + words
+
+    return test_words
+
+
+def verify():
+    pass
+
+
+print(get_common_words())
+print(get_test_words())
